c294c32f33e00c9ddc1a0fcde187b622ae6a6b01
Anand Mishra <anandmishra71996@gmail.com>
2024-01-06 20:26:45 +0530

    scaler ownership

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::ops::Add;
+
 fn main() {
-    println!("Hello, world!");
+    let mut val = 6;
+    add_two(val);
+    println!("{}", val); // we can pass the val variable because i32 implement copy traits which does clone of scaler variables
 }
+fn add_two(mut val: i32) {
+    val += 2;
+}
+// fn add_two(val: &mut i8) -> i8 {}
