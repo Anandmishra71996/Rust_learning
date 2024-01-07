2e3502ea167d9bde9af4805877626fa87f8a4a07
Anand Mishra <anandmishra71996@gmail.com>
2024-01-07 15:40:03 +0530

    enum and impl on enum

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
-struct Car {
-    mpg: i32,
-    color: String,
-    top_speed: i32,
+enum shape {
+    Triangle,
+    Square,
+    Pentagon,
+    Octagon,
 }
-impl Car {
-    fn set_mpg(&mut self, mpg: i32) {
-        self.mpg = mpg;
-    }
-    fn set_color(&mut self, color: String) {
-        self.color = color;
-    }
-    fn set_top_speed(&mut self, topSpeed: i32) {
-        self.top_speed = topSpeed;
+impl shape {
+    fn corners(&self) -> i8 {
+        match self {
+            shape::Triangle => 3,
+            shape::Square => 4,
+            shape::Pentagon => 5,
+            shape::Octagon => 8,
+            _ => 0,
+        }
     }
 }
-
 fn main() {
-    let mut car = Car {
-        mpg: 120,
-        color: String::from("Red"),
-        top_speed: 210,
-    };
-    car.set_mpg(150);
-    car.set_color(String::from("Blue"));
-    car.set_top_speed(220);
-    println!("{:?} {} {}", car.color, car.mpg, car.top_speed);
+    let sq = shape::Square;
+    let tr = shape::Triangle;
+    let val = sq.corners();
+    let val1 = tr.corners();
+    println!("{}", val);
+    println!("{}", val1);
 }
-
-// fn add_two(val: &mut i8) -> i8 {}
