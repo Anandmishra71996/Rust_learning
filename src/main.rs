3e69b76e63af01602ea4e81fe218ad1c325b97e0
Anand Mishra <anandmishra71996@gmail.com>
2024-01-07 15:28:34 +0530

    struct implemented

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::ops::Add;
+struct Car {
+    mpg: i32,
+    color: String,
+    top_speed: i32,
+}
+impl Car {
+    fn set_mpg(&mut self, mpg: i32) {
+        self.mpg = mpg;
+    }
+    fn set_color(&mut self, color: String) {
+        self.color = color;
+    }
+    fn set_top_speed(&mut self, topSpeed: i32) {
+        self.top_speed = topSpeed;
+    }
+}
 
 fn main() {
-    let mut val = 6;
-    add_two(val);
-    println!("{}", val); // we can pass the val variable because i32 implement copy traits which does clone of scaler variables
+    let mut car = Car {
+        mpg: 120,
+        color: String::from("Red"),
+        top_speed: 210,
+    };
+    car.set_mpg(150);
+    car.set_color(String::from("Blue"));
+    car.set_top_speed(220);
+    println!("{:?} {} {}", car.color, car.mpg, car.top_speed);
 }
-fn add_two(mut val: i32) {
-    val += 2;
-}
+
 // fn add_two(val: &mut i8) -> i8 {}
